f13c4a505cd96caf85c90185f9cd6d47a66fcf33
Noha-A-Thabet <nohaashraf25@outlook.com>
2023-10-16 12:55:08 +0200

    add to cart by Redux-Toolkit

--- src/Components/Mobiles/MobileList.jsx
+++ src/Components/Mobiles/MobileList.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Mobiles.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../Redux/cartSlice";
 
 const MobileList = ({ mobile }) => {
   const { name, price, image } = mobile;
+
+  const cartItems = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(mobile));
+  };
 
   return (
     <div>
@@ -19,6 +28,7 @@ const MobileList = ({ mobile }) => {
           <h2>{name}</h2>
 
           <p className={classes.price}>${price}</p>
+          <button onClick={addToCartHandler}>Add To Cart</button>
         </div>
       </div>
       {/* //// */}
