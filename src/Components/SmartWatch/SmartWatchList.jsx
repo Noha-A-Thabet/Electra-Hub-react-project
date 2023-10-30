423927ca2e1131bfd7b8f42f06fb9a99712dfb71
Noha-A-Thabet <nohaashraf25@outlook.com>
2023-10-30 11:17:58 +0100

    style Wateches bar

--- src/Components/SmartWatch/SmartWatchList.jsx
+++ src/Components/SmartWatch/SmartWatchList.jsx
@@ -1,10 +1,10 @@
 import classes from "./SmartWatch.module.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addToCart } from "../Redux/cartSlice";
+import { addToFav } from "../Redux/favouriteSlice";
 
 const WatchesList = ({ watch }) => {
   const { name, price, image } = watch;
-  const cartItems = useSelector((state) => state.cart.cartItems);
 
   const dispatch = useDispatch();
 
@@ -12,10 +12,22 @@ const WatchesList = ({ watch }) => {
     dispatch(addToCart(watch));
   };
 
+  const whishListHandler = () => {
+    dispatch(addToFav(watch));
+  };
+
   return (
     <div>
       <div className={classes.parent}>
         <div className={classes.child}>
+          <div className={classes.whishListDiv}>
+            <button
+              onClick={whishListHandler}
+              className={classes.whistListIcon}
+            >
+              <i class="fa-solid fa-heart"></i>
+            </button>
+          </div>
           <img src={image} alt="" />
           <span className={classes.rating}>
             <i class="stars fa-solid fa-star"></i>
@@ -25,9 +37,10 @@ const WatchesList = ({ watch }) => {
             <i class="stars last fa-solid fa-star"></i>
           </span>
           <h2>{name}</h2>
-
           <p className={classes.price}>${price}</p>
-          <button onClick={addToCartHandler}>Add To Cart</button>
+          <button onClick={addToCartHandler} className={classes.addBtn}>
+            Add To Cart
+          </button>
         </div>
       </div>
     </div>
