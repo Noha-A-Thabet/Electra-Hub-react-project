78da48fb042c288f09bb89dffe9630c7498ae072
Noha-A-Thabet <nohaashraf25@outlook.com>
2023-10-30 11:05:16 +0100

    style laptop section

--- src/Components/Laptop/LaptopList.jsx
+++ src/Components/Laptop/LaptopList.jsx
@@ -1,6 +1,7 @@
 import classes from "./Laptop.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../Redux/cartSlice";
+import { addToFav } from "../Redux/favouriteSlice";
 
 const LaptopList = ({ laptop }) => {
   const { name, price, image } = laptop;
@@ -12,10 +13,22 @@ const LaptopList = ({ laptop }) => {
     dispatch(addToCart(laptop));
   };
 
+  const whishListHandler = () => {
+    dispatch(addToFav(laptop));
+  };
+
   return (
     <div>
       <div className={classes.parent}>
         <div className={classes.child}>
+          <div className={classes.whishListDiv}>
+            <button
+              onClick={whishListHandler}
+              className={classes.whistListIcon}
+            >
+              <i class="fa-solid fa-heart"></i>
+            </button>
+          </div>
           <img src={image} alt="" />
           <span className={classes.rating}>
             <i class="stars fa-solid fa-star"></i>
@@ -25,9 +38,10 @@ const LaptopList = ({ laptop }) => {
             <i class="stars last fa-solid fa-star"></i>
           </span>
           <h2>{name}</h2>
-
           <p className={classes.price}>${price}</p>
-          <button onClick={addToCartHandler}>Add To Cart</button>
+          <button onClick={addToCartHandler} className={classes.addBtn}>
+            Add To Cart
+          </button>
         </div>
       </div>
     </div>
